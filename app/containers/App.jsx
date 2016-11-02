d346634fe2e833f374822535413ba4f962c13b35
younthxg@gmail.com <younthxg@gmail.com>
2016-11-02 19:07:50 +0800

    edit promise

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -7,7 +7,24 @@ import { hashHistory } from 'react-router'
 
 import * as demoActions from '../actions/demo'
 
-class App extends Component {
+// 将 store 中的数据作为 props 绑定到组件上，参数就是 Redux 的 store
+function mapStateToProps(state) {
+    return {
+        demo: state.demo
+    }
+}
+
+// 将 action 作为 props 绑定到 组件上 要触发这个 action 必须在 store 上调用 dispatch 方法
+// bindActionCreators 将 action 包装成直接可被调用的函数 diapatch 正是 mapDispatchToProps 的第一个参数
+// 调用该方法就会触发 dispatch
+function mapDispatchToProps(dispatch) {
+    return {
+        demoActions: bindActionCreators(demoActions, dispatch)
+    }
+}
+
+@connect(mapStateToProps,mapDispatchToProps)
+export default class App extends Component {
 
     static propTypes = {
         demo: PropTypes.object.isRequired,
@@ -30,7 +47,8 @@ class App extends Component {
     }
 
     componentDidMount() {
-       
+        // 只会执行一次，合适公共数据请求
+       console.log('layout Didmount');
     }
 
     // 路由切换时会触发
@@ -39,26 +57,3 @@ class App extends Component {
         window.webappLocationFrom = location.hash;
     }
 }
-
-// -------------------redux react 绑定--------------------
-
-// 将 store 中的数据作为 props 绑定到组件上，参数就是 Redux 的 store
-function mapStateToProps(state) {
-    return {
-        demo: state.demo
-    }
-}
-
-// 将 action 作为 props 绑定到 组件上 要触发这个 action 必须在 store 上调用 dispatch 方法
-// bindActionCreators 将 action 包装成直接可被调用的函数 diapatch 正是 mapDispatchToProps 的第一个参数
-// 调用该方法就会触发 dispatch
-function mapDispatchToProps(dispatch) {
-    return {
-        demoActions: bindActionCreators(demoActions, dispatch)
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(App)
